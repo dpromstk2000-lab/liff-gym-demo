1a2ca297a57ab478dd63fa342ed9c0043a26dfeb
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-16 16:09:47 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+/* STEP GYM-1 / DPRO パーソナルジム LINE */
+window.DPRO_GYM_CONFIG = Object.freeze({
+  version: 'GYM-1-CONFIG-20260716',
+  facilityCode: 'dpro_gym_demo',
+  workerBaseUrl: 'https://dpro-gym-line-api.dpromstk2000.workers.dev',
+  publicSiteUrl: 'https://dpromstk2000-lab.github.io/liff-gym-demo/',
+  demoAdminCode: '1234',
+  timezone: 'Asia/Tokyo',
+  slotMinutes: 30
+});
